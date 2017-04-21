9f302897ebb88a46a13886d49a5be9c9e8fe28fe
Robert Jones <robert.g.jones@gmail.com>
2017-04-21 16:42:49 +0100

    spec/journey_spec.rbcomplete_journey changes @journey_complete to true

--- lib/journey.rb
+++ lib/journey.rb
@@ -1,10 +1,20 @@
 class Journey
 
-  attr_reader :entry_station, :journey_complete
+  attr_reader :entry_station, :exit_station, :journey_complete
 
   def initialize(arg)
     @journey_complete = false
     @entry_station = arg
   end
 
+  def complete_journey(exit_station)
+    self.journey_complete = true
+  end
+
+  def journey_complete?
+    journey_complete
+  end
+
+  private
+  attr_writer :journey_complete
 end
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -2,14 +2,23 @@ require 'journey'
 
 describe Journey do
   let (:entry_station) { double(:station, :name => "Blackfriars!", :zone => 2) }
-  subject { described_class.new(entry_station) }
+  let (:exit_station) { double(:station, :name => "Montreal!", :zone => 30) }
+  subject(:journey) { described_class.new(entry_station) }
 
   it 'has an entry station' do
-    expect(subject.entry_station).to eq entry_station
+    expect(journey.entry_station).to eq entry_station
   end
 
   it 'is not complete' do
-    expect(subject.journey_complete).to be false
+    expect(journey.journey_complete).to be false
   end
 
+  it { is_expected.to respond_to :exit_station }
+
+  describe '#complete_journey' do
+    it 'sets #journey_complete true' do
+      journey.complete_journey(exit_station)
+      expect(journey.journey_complete).to be true
+    end
+  end
 end

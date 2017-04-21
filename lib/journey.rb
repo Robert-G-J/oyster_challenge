3814be71b15b59bb68d5396704e167236be10281
Alex Chalk <alexchalk17@gmail.com>
2017-04-21 09:51:52 -0400

    Saves init arg as @entry_station; journey_complete inits as false

--- lib/journey.rb
+++ lib/journey.rb
@@ -1,2 +1,10 @@
 class Journey
+
+  attr_reader :entry_station, :journey_complete
+
+  def initialize(arg)
+    @journey_complete = false
+    @entry_station = arg
+  end
+
 end
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -1,4 +1,15 @@
 require 'journey'
 
 describe Journey do
+  let (:entry_station) { double(:station, :name => "Blackfriars!", :zone => 2) }
+  subject { described_class.new(entry_station) }
+
+  it 'has an entry station' do
+    expect(subject.entry_station).to eq entry_station
+  end
+
+  it 'is not complete' do
+    expect(subject.journey_complete).to be false
+  end
+
 end
